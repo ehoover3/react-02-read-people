0ee0dafbc0526913713796c60af2ba2e9ce4611a
Eric Hoover <ehoover3@yahoo.com>
2021-04-08 12:52:43 -0400

    added Reddit API and Ibe API

---
+++ src/components/sidebar/Ibe.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Accordion, Button, Card } from "react-bootstrap";
+
+// https://github.com/public-apis/public-apis
+
+function Ibe() {
+  // useEffect(() => {}, []);
+
+  // Add the API fetch request here
+
+  // Add other functions here
+
+  return (
+    <Accordion>
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            Ibe API
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>
+            <section>ADD FUNCTIONS OR TEXT HERE</section>
+          </Card.Body>
+        </Accordion.Collapse>
+      </Card>
+    </Accordion>
+  );
+}
+export default Ibe;
--- src/components/sidebar/SideBar.js
+++ src/components/sidebar/SideBar.js
@@ -1,13 +1,16 @@
 import React from "react";
 import AllUsersNames from "./AllUsersNames.js";
 import TopLikedMessages from "./TopLikedMessages.js";
+import RedditAPI from "./RedditAPI.js";
+import Ibe from "./Ibe.js";
 
 function SideBar(props) {
   return (
     <>
       <AllUsersNames />
       <TopLikedMessages />
-      {/* <Feeds /> */}
+      <RedditAPI>{/* <Feeds /> */}</RedditAPI>
+      <Ibe />
     </>
   );
 }
