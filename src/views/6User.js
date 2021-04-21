c64017f0897948f66fab128985d79102c78bd25b
Eric Hoover <ehoover3@yahoo.com>
2021-04-21 10:55:39 -0400

    cut project items

--- src/views/6User.js
+++ src/views/6User.js
@@ -1,11 +1,14 @@
 import React from "react";
 import About from "../components/6User/About";
-import Photo from "../components/6User/Photo";
+// import Photo from "../components/6User/Photo";
 
 function User() {
   return (
     <>
-      <Photo />
+      {/* To have no bugs for the demo, we're cutting Photo */}
+      {/* We have the needed requirements for the rubric */}
+      {/* Connecting photo to the backend will happen at a later date in time */}
+      {/* <Photo /> */}
       <About />
     </>
   );
